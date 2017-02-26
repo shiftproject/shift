8c3098ba56b71a1c540a2df451269ac93203d876
TheGoldenEye <GoldenEye@jn11.xyz>
2017-02-26 23:26:43 +0100

    upgrade to Lisk 0.6.0 codebase

--- test/api/delegates.js
+++ test/api/delegates.js
@@ -629,6 +629,33 @@ describe('GET /api/delegates', function () {
 			done();
 		});
 	});
+
+	it('using orderBy with any of sort fields should not place NULLs first', function (done) {
+		var delegatesSortFields = ['approval', 'productivity', 'rate', 'vote'];
+		node.async.each(delegatesSortFields, function (sortField, cb) {
+			node.get('/api/delegates?orderBy=' + sortField, function (err, res) {
+				node.expect(res.body).to.have.property('success').to.be.ok;
+				node.expect(res.body).to.have.property('delegates').that.is.an('array');
+
+				var dividedIndices = res.body.delegates.reduce(function (memo, peer, index) {
+					memo[peer[sortField] === null ? 'nullIndices' : 'notNullIndices'].push(index);
+					return memo;
+				}, {notNullIndices: [], nullIndices: []});
+
+				if (dividedIndices.nullIndices.length && dividedIndices.notNullIndices.length) {
+					var ascOrder = function (a, b) { return a - b; };
+					dividedIndices.notNullIndices.sort(ascOrder);
+					dividedIndices.nullIndices.sort(ascOrder);
+
+					node.expect(dividedIndices.notNullIndices[dividedIndices.notNullIndices.length - 1])
+						.to.be.at.most(dividedIndices.nullIndices[0]);
+				}
+				cb();
+			});
+		}, function () {
+			done();
+		});
+	});
 });
 
 describe('GET /api/delegates/count', function () {
@@ -959,10 +986,11 @@ describe('GET /api/delegates/search', function () {
 });
 
 describe('GET /api/delegates/forging/status', function () {
-	it('using no params should fail', function (done) {
+	it('using no params should be ok', function (done) {
 		node.get('/api/delegates/forging/status', function (err, res) {
-			node.expect(res.body).to.have.property('success').to.be.not.ok;
-			node.expect(res.body).to.have.property('error').to.eql('Missing required property: publicKey');
+			node.expect(res.body).to.have.property('success').to.be.ok;
+			node.expect(res.body).to.have.property('enabled').to.be.true;
+			node.expect(res.body).to.have.property('delegates').that.is.an('array');
 			done();
 		});
 	});
@@ -978,7 +1006,8 @@ describe('GET /api/delegates/forging/status', function () {
 	it('using empty publicKey should be ok', function (done) {
 		node.get('/api/delegates/forging/status?publicKey=', function (err, res) {
 			node.expect(res.body).to.have.property('success').to.be.ok;
-			node.expect(res.body).to.have.property('enabled').to.be.false;
+			node.expect(res.body).to.have.property('enabled').to.be.true;
+			node.expect(res.body).to.have.property('delegates').that.is.an('array');
 			done();
 		});
 	});
@@ -1137,6 +1166,7 @@ describe('GET /api/delegates/getNextForgers', function () {
 		node.get('/api/delegates/getNextForgers', function (err, res) {
 			node.expect(res.body).to.have.property('success').to.be.ok;
 			node.expect(res.body).to.have.property('currentBlock').that.is.a('number');
+			node.expect(res.body).to.have.property('currentBlockSlot').that.is.a('number');
 			node.expect(res.body).to.have.property('currentSlot').that.is.a('number');
 			node.expect(res.body).to.have.property('delegates').that.is.an('array');
 			node.expect(res.body.delegates).to.have.lengthOf(10);
@@ -1148,6 +1178,7 @@ describe('GET /api/delegates/getNextForgers', function () {
 		node.get('/api/delegates/getNextForgers?' + 'limit=1', function (err, res) {
 			node.expect(res.body).to.have.property('success').to.be.ok;
 			node.expect(res.body).to.have.property('currentBlock').that.is.a('number');
+			node.expect(res.body).to.have.property('currentBlockSlot').that.is.a('number');
 			node.expect(res.body).to.have.property('currentSlot').that.is.a('number');
 			node.expect(res.body).to.have.property('delegates').that.is.an('array');
 			node.expect(res.body.delegates).to.have.lengthOf(1);
@@ -1159,6 +1190,7 @@ describe('GET /api/delegates/getNextForgers', function () {
 		node.get('/api/delegates/getNextForgers?' + 'limit=101', function (err, res) {
 			node.expect(res.body).to.have.property('success').to.be.ok;
 			node.expect(res.body).to.have.property('currentBlock').that.is.a('number');
+			node.expect(res.body).to.have.property('currentBlockSlot').that.is.a('number');
 			node.expect(res.body).to.have.property('currentSlot').that.is.a('number');
 			node.expect(res.body).to.have.property('delegates').that.is.an('array');
 			node.expect(res.body.delegates).to.have.lengthOf(101);
