d92e60a30c04ed09652fec5fd573056caf95e322
TheGoldenEye <GoldenEye@jn11.xyz>
2017-04-08 19:13:03 +0200

    get build date from last commit

--- helpers/git.js
+++ helpers/git.js
@@ -26,6 +26,26 @@ function getLastCommit () {
 	}
 }
 
+/**
+ * Return date of a given commit if available
+ *
+ * @method getCommitDate
+ * @public
+ * @param {String} commit (optional, last commit, if not given)
+ * @return {String} Date of last git commit
+ * @throws {Error} Throws error if cannot get the date of git commit
+ */
+function getCommitDate ( commit ) {
+	var spawn = childProcess.spawnSync('git', ['show', '-s', '--format=%ci', commit || 'HEAD']);
+	var err = spawn.stderr.toString().trim();
+	if (err) {
+		throw new Error(err);
+	} else {
+		return spawn.stdout.toString().trim();
+	}
+}
+
 module.exports = {
-	getLastCommit: getLastCommit
+	getLastCommit: getLastCommit,
+	getCommitDate: getCommitDate
 };
