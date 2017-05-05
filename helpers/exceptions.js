71e0d7e8ce8193f26b1e4cc492c2e5480ab59f7a
TheGoldenEye <GoldenEye@jn11.xyz>
2017-05-05 21:10:08 +0200

    upgrade to Lisk 0.8.2 codebase

--- helpers/exceptions.js
+++ helpers/exceptions.js
@@ -5,6 +5,8 @@ module.exports = {
 		mainnet: '3c4bd532ef8a90c0105f1b21458abf652b1e7ca100f664c39a3bbfc2e682efef',
 		testnet: '03f5d49d8bf116e59c2306d40f03c35fcb6a8ac12932203460c8ece4d69ea0f4'
 	},
+	rounds: {
+	},
 	senderPublicKey: [
 	],
 	signatures: [
