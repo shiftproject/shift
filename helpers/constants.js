a59e3474ea347a463e57140a48d8e120779faf8a
ralfshift <ralfs@shiftnrg.org>
2018-04-28 13:52:32 +0200

    Bump version

--- helpers/constants.js
+++ helpers/constants.js
@@ -41,7 +41,7 @@
  * @property {number} unconfirmedTransactionTimeOut - 1080 blocks
  */
 module.exports = {
-	currentVersion: '6.7.1t',
+	currentVersion: '6.7.2t',
 	minVersion: [
 		{ height: 1,      ver: '^6.0.1t'},
 		{ height: 370000, ver: '^6.3.0t'},
