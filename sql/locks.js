c32dcf2f5ef9830d3fc126ec533750f61e170c73
ralfshift <ralfs@shiftnrg.org>
2019-03-06 15:14:17 +0100

    Remove redundant db colums

--- sql/locks.js
+++ sql/locks.js
@@ -9,9 +9,9 @@ var LocksSql = {
 
 	getClusterStats: 'SELECT * FROM mem_cluster_stats ORDER BY "stats_timestamp" DESC LIMIT ${limit}',
 	setClusterStats: 'INSERT INTO mem_cluster_stats '+
-	'("id","total_locked_balance","total_unlocked_balance","total_locked_bytes","total_unlocked_bytes","latest_cluster_total","latest_cluster_used","stats_timestamp") '+
-	'VALUES (${id}, ${locked_balance}, ${unlocked_balance}, ${locked_bytes}, ${unlocked_bytes}, ${total_bytes}, ${used_bytes}, ${timestamp}) '+
-	'ON CONFLICT (id) DO UPDATE set "total_locked_balance" = ${locked_balance}, "total_unlocked_balance" = ${unlocked_balance}, "total_locked_bytes" = ${locked_bytes}, "total_unlocked_bytes" = ${unlocked_bytes}, "latest_cluster_total" = ${total_bytes}, "latest_cluster_used" = ${used_bytes}, "stats_timestamp" = ${timestamp}' 
+	'("id","total_locked_balance","total_locked_bytes","latest_cluster_total","latest_cluster_used","stats_timestamp") '+
+	'VALUES (${id}, ${locked_balance}, ${locked_bytes}, ${total_bytes}, ${used_bytes}, ${timestamp}) '+
+	'ON CONFLICT (id) DO UPDATE set "total_locked_balance" = ${locked_balance}, "total_locked_bytes" = ${locked_bytes}, "latest_cluster_total" = ${total_bytes}, "latest_cluster_used" = ${used_bytes}, "stats_timestamp" = ${timestamp}' 
 };
 
 module.exports = LocksSql;
--- sql/migrations/20180829094741_addMemClusterStats.sql
+++ sql/migrations/20180829094741_addMemClusterStats.sql
@@ -7,9 +7,7 @@ BEGIN;
 CREATE TABLE IF NOT EXISTS "mem_cluster_stats"(
   "id" VARCHAR(22) NOT NULL PRIMARY KEY,
   "total_locked_balance" BIGINT NOT NULL DEFAULT(0),
-  "total_unlocked_balance" BIGINT NOT NULL DEFAULT(0),
   "total_locked_bytes" BIGINT NOT NULL DEFAULT(0),
-  "total_unlocked_bytes" BIGINT NOT NULL DEFAULT(0),
   "latest_cluster_total" BIGINT NOT NULL DEFAULT(0),
   "latest_cluster_used" BIGINT NOT NULL DEFAULT(0),
   "stats_timestamp" INT
